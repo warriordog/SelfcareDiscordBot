ba3265b62995425591334f3e7b55a063adb63f1e
Chris Koehler <acomputerdog@gmail.com>
2021-01-09 18:11:16 -0500

    Remove @mentions from leaderboard

--- SelfcareBot/Commands/CmdHydrate.cs
+++ SelfcareBot/Commands/CmdHydrate.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading.Tasks;
 using DSharpPlus;
@@ -77,36 +78,50 @@ namespace SelfcareBot.Commands
             {
                 _logger.LogDebug("Requested by [{user}]", ctx.User);
 
-                // Setup leaderboard message
-                var leaderboardLines = new List<string>
-                {
-                    "Hydration leaderboard:"
-                };
-
-                // Append leaderboard rows to message
+                // Create leaderboard text
+                var leaderboardLines = new List<string>();
                 var leaderboard = (await _hydrationLeaderboard.GetLeaderboard(_hydrationOptions.LeaderboardSize)).ToArray();
                 if (leaderboard.Any())
                 {
                     foreach (var entry in leaderboard)
                     {
-                        var user = await ctx.Client.GetUserAsync(entry.UserId);
-                        var userMention = Formatter.Mention(user, true);
-                        var line = $"{entry.Rank.ToString()}. {userMention}: {entry.Score.ToString()}";
+                        var memberName = await GetUserDisplayName(entry.UserId, ctx.Client, ctx.Guild);
+                        var line = $"{entry.Rank.ToString()}: {memberName} ({entry.Score.ToString()})";
                         leaderboardLines.Add(line);
                     }
                 }
                 else
                 {
-                    leaderboardLines.Add($"No results! Use {Formatter.InlineCode("care*hydrate")} to call for hydration.");
+                    leaderboardLines.Add($"No results! Use the hydrate command to call for hydration.");
                 }
 
+                // Convert to inline code style
+                var leaderboardText = Formatter.BlockCode(string.Join("\n", leaderboardLines));
+                
                 // Debug log leaderboard
                 _logger.LogDebug("leaderboard entries: [{leaderboard}]", new object[]{ leaderboard });
 
                 // Send message to channel
-                var leaderboardMessage = string.Join("\n", leaderboardLines);
+                var leaderboardMessage = $"Hydration leaderboard:\n{leaderboardText}";
                 await ctx.RespondAsync(leaderboardMessage);
             }
+        }
+
+        private static async Task<string> GetUserDisplayName(ulong userId, DiscordClient client, [AllowNull] DiscordGuild guild = null)
+        {
+            // Get from guild, if possible
+            if (guild != null)
+            {
+                var member = await guild.GetMemberAsync(userId);
+                if (member != null)
+                {
+                    return $"{member.DisplayName}#{member.Discriminator}";
+                }
+            }
+            
+            // Otherwise fall back to global
+            var user = await client.GetUserAsync(userId);
+            return $"{user.Username}#{user.Discriminator}";
         }
     }
 }
