3a4ad863a0da73f3b51fbaa42814fcd2816249c4
Chris Koehler <acomputerdog@gmail.com>
2021-01-08 20:58:19 -0500

    Add basic project structure

---
+++ SelfcareBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SelfcareBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
